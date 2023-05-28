ddd39a21105360667aa824f24b796aa3f5e76d87
namnguyen0510 <64614275+namnguyen0510@users.noreply.github.com>
2023-05-27 23:31:22 -0400

    Update primitives.py

--- primitives.py
+++ primitives.py
@@ -4,7 +4,7 @@ import numpy as np
 
 dev_1 = qml.device("default.qubit", wires=4)
 @qml.qnode(dev_1)
-def ASymmetricDNAEncoder(X, div_2 = False):
+def ASymmetricDNAEncoder(X):
     qml.Rot(phi =X[0], theta = X[1], omega = X[2], wires = 0)
     qml.Rot(phi =X[1], theta = X[2], omega = X[0], wires = 1)
     qml.Rot(phi =X[2], theta = X[0], omega = X[1], wires = 2)
@@ -17,7 +17,7 @@ def ASymmetricDNAEncoder(X, div_2 = False):
 
 dev_2 = qml.device("default.qubit", wires=4)
 @qml.qnode(dev_2)
-def SymmetricDNAEncoder(X, div_2 = False):
+def SymmetricDNAEncoder(X):
     qml.Rot(phi =X[0], theta = X[1], omega = X[2], wires = 0)
     qml.Rot(phi =X[2], theta = X[0], omega = X[1], wires = 1)
     qml.Rot(phi =X[1], theta = X[2], omega = X[0], wires = 2)
